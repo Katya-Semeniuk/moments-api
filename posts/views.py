be953cdf1c53f78eba96e64af3ffdb1aaf45ac36
Kateryna Semeniuk <katyasemeniuk@gmail.com>
2024-08-30 13:33:17 +0000

    added text search functionality to user posts

--- posts/views.py
+++ posts/views.py
@@ -15,11 +15,17 @@ class PostList(generics.ListCreateAPIView):
     ]
     queryset = Post.objects.annotate(
         comments_count = Count('comment', distinct=True),
-        likes_count=Count('likes', distinct=True)
+        likes_count = Count('likes', distinct=True)
     ).order_by('-created_at')
 
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+
+    search_fields = [
+        'owner__username',
+        'title',
     ]
 
     ordering_fields = [
